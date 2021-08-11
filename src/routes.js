164928d73aa0bef277c13018050ba1577f703f93
Vladyslav Bochkovskyi <bochkovskyi.vladyslav@gmail.com>
2021-08-12 00:48:44 +0300

    3222

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  home: '/',
+  home: '/goit-react-hw-07-phonebook',
   contacts: '/contacts',
   login: '/login',
   register: '/registration',
